4ccdb265332ea9ef35de80c4248c0a76dcb64ccc
Macwdo <danilo.macedofernandes@hotmail.com>
2025-05-24 00:25:01 -0300

    chore: gpt update patch 1.0

--- celery.sh
+++ celery.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+
 set -e
 
-adduser -D -h /home/celery celery
-chown -R celery:celery /app
-exec su-exec celery uv run celery -A core worker --loglevel=info --concurrency=2
+# Only add user if it doesn't already exist
+id -u celery 2>/dev/null || adduser -D -h /home/celery celery
+
+# Make sure .venv is owned by celery
+chown -R celery:celery .venv
+
+# Run as celery user (ensure su-exec is installed)
+exec su-exec celery .venv/bin/celery -A core worker --loglevel=info --concurrency=2
